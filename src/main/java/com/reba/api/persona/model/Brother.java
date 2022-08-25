a5679d4b5d568208f999a62c440128fcaf507705
marianoponte <mariano.ponte@hotmail.com>
2022-08-25 13:03:12 -0300

    Prueba refactor

---
+++ src/main/java/com/reba/api/persona/model/Brother.java
@@ -0,0 +1,14 @@
+package com.reba.api.persona.model;
+
+import com.reba.api.persona.utils.Constants;
+
+public abstract class Brother {
+
+    public static String exist(Person persona1, Person persona2) {
+        if (persona1.getFather() != null
+                && persona2.getFather() != null
+                && persona1.getFather().equals(persona2.getFather())) {
+            return Constants.HERMANO;
+        } else return null;
+    }
+}
